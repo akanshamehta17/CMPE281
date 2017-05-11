8dfe6fc01c0b3a5bc49491b94e6a3a3781cbaa69
akanshamehta17 <akansha.mehta@sjsu.edu>
2017-05-11 03:52:01 -0700

    Update grading_page.php

--- Tenant-3/grading_page.php
+++ Tenant-3/grading_page.php
@@ -1,27 +1,40 @@
 <?php
+
 include "../../inc/dbinfo.inc";
 $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
   if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
   $database = mysqli_select_db($connection, DB_DATABASE);
-  VerifyTenantsTable($connection, DB_DATABASE);
+
+
+  VerifyTenantTable($connection, DB_DATABASE);
+
   $Name = htmlentities($_POST['name']);
   $Id = htmlentities($_POST['id']);
   $Grade = htmlentities($_POST['grade']);
   $Status = htmlentities($_POST['status']);
-  if (strlen($Name) || strlen($Id) || strlen($Grade) ||  strlen($Status)) {
-    AddTenantGrades($connection, $Name, $Id, $Grade, $Status);
+  $Comments = htmlentities($_POST['comments']);
+
+  if (strlen($Name) || strlen($Id) ||  strlen($Grade) || strlen($Status) || strlen($Comments)) {
+    AddTenantGrades($connection, $Name, $Id, $Grade, $Status, $Comments);
   }
- /* Add tenant to the table. */
-function AddTenantGrades($connection, $Name, $Id, $Grade, $Status) {
+
+
+ /* Add Tenant's data to the table. */
+function AddTenantGrades($connection, $Name, $Id, $Grade, $Status, $Comments) {
    $n = mysqli_real_escape_string($connection, $Name);
    $i = mysqli_real_escape_string($connection, $Id);
    $g = mysqli_real_escape_string($connection, $Grade);
    $s = mysqli_real_escape_string($connection, $Status);
-   $query = "INSERT INTO `Tenant_Data` (`Tenant_Name`, `TENANT_ID`, `Column_1`, `Column_2`, `Column_3`, `Column_4`, `Column_5`, `Column_6`) VALUES ('$n', $i, '$s', '$g', 'null', 'null', 'null', 'null')";
-   if(!mysqli_query($connection, $query)) echo("<p>Error adding Tenant data.</p>");
+   $c = mysqli_real_escape_string($connection, $Comments);
+
+   $query = "INSERT INTO `Tenant_Data` (`Tenant_Name`, `TENANT_ID`, `Column_1`, `Column_2`, `Column_3`, `Column_4`, `Column_5`,`Column_6`) VALUES ('$n', $i, '$g', '$s', '$c', 'null', 'null', 'null')";
+
+   if(!mysqli_query($connection, $query)) echo("<p>Error adding tenant data.</p>");
 }
 /* Check whether the table exists and, if not, create it. */
-function VerifyTenantsTable($connection, $dbName) {
+function VerifyTenantTable($connection, $dbName) {
   if(!TableExists("Tenant_Data", $connection, $dbName))
   {
      $query = "CREATE TABLE `Tenant_Data` (
@@ -36,16 +49,21 @@ function VerifyTenantsTable($connection, $dbName) {
          `Column_6` varchar(200) NULL,
          PRIMARY KEY (`Record_ID`)
        ) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
      if(!mysqli_query($connection, $query)) echo("<p>Error creating table.</p>");
   }
 }
+
 /* Check for the existence of a table. */
 function TableExists($tableName, $connection, $dbName) {
   $t = mysqli_real_escape_string($connection, $tableName);
   $d = mysqli_real_escape_string($connection, $dbName);
+
   $checktable = mysqli_query($connection,
       "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
+
   if(mysqli_num_rows($checktable) > 0) return true;
+
   return false;
 }
 ?>
@@ -66,9 +84,12 @@ Grade:
 <input type="radio" name="status" value="Not Completed"> Not Complete
 &nbsp;&nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;
+<br><br>Comments:&nbsp;<textarea name="comments" id="comments" style="font-family:sans-serif;font-size:1.2em;">
+</textarea>
 <br><br><br>
 <input type="submit" value="Submit"><br><br>
 </form>
 </body>
 </head>
 </html>
+
