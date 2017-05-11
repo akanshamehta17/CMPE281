770303ec824deececf4ed5c66604f811eea16e21
akanshamehta17 <akansha.mehta@sjsu.edu>
2017-05-11 03:20:14 -0700

    Create out.php

---
+++ Tenant-2/out.php
@@ -0,0 +1,14 @@
+<?php
+
+shell_exec("java -jar umlparser.jar /var/www/html/Tenant-2/ /var/www/html/Tenant-2/outputImg.png");
+
+ ?>
+
+
+<html>
+<body bgcolor="#ADD8E6">
+<b><h2>Output:</h2></b><br><br>
+<img src="outputImg.png" width="600" height="650" />
+<a href="upload.php"><h2><b>Back</h2></b></a>
+</body>
+</html>
